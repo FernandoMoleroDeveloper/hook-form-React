f4a0edc70e7f54caf0201ba4f2f93ee7ff547eee
FernandoMoleroDeveloper <118685226+FernandoMoleroDeveloper@users.noreply.github.com>
2023-03-22 22:00:16 +0100

    r

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import RegisterForm from './components/ResgisterForm/ResgisterForm';
+import Spanish from './lang/es.json';
+import French from './lang/fr.json';
+import English from './lang/en.json';
+import { IntlProvider, FormattedMessage } from 'react-intl';
+import React from 'react';
+
+const locale = navigator.language;
+let defaultMessages = Spanish;
+switch (locale) {
+  case 'fr-FR':
+    defaultMessages = French;
+    break;
+  case 'es-ES':
+    defaultMessages = Spanish;
+    break;
+  case 'en-EN':
+    defaultMessages = English;
+    break;
+  default:
+    defaultMessages = Spanish;
+}
 
 function App() {
+  const [messages, setMessages] = React.useState(defaultMessages);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IntlProvider locale={locale} messages={messages}>
+      <div className='app'>
+        <RegisterForm></RegisterForm>
+        <h2>Selector de idioma</h2>
+        <button onClick={() => setMessages(Spanish)}>
+          <FormattedMessage id='app:spanish' />
+        </button>
+        <button onClick={() => setMessages(English)}>
+          <FormattedMessage id='app:english' />
+        </button>
+        <button onClick={() => setMessages(French)}>
+          <FormattedMessage id='app:french' />
+        </button>
+      </div>
+    </IntlProvider>
   );
 }
 
 export default App;
+
+/*  <select onChange={event => {
+<option value='es-ES'>Spanish</option>
+<option value='en-EN'>English</option>
+<option value='fr-FR'>French</option>
+        }}>
+
+          </select> */
